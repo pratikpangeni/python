eb417fa65cc5544b0e699ac0c0d1b9b38ad2db23
pratikpangeni <46888506+pratikpangeni@users.noreply.github.com>
2019-01-22 16:14:49 +0530

    Create arthematicoperation.py

---
+++ arthematicoperation.py
@@ -0,0 +1,11 @@
+#addition of two number
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+sum = a + b
+mul=a*b
+div=a/b
+sub=a-b
+print("sum:", sum)
+print("div:", div)
+print("mul:", mul)
+print("sub:", sub)
